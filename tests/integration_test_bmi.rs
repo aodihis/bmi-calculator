ee5bde27c7120d1f4e139d0eaba5554be621450d
Iqbal <aodihis@gmail.com>
2025-02-21 05:10:47 +0700

    Fmt clean up.

--- tests/integration_test_bmi.rs
+++ tests/integration_test_bmi.rs
@@ -5,7 +5,6 @@
 //! 2. Invalid Parameters.
 use actix_web::{test, App};
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -16,19 +15,19 @@ mod tests {
 
     #[actix_web::test]
     async fn test_bmi_endpoint() {
-
-        let send_req =  |payload, app| {
+        let send_req = |payload, app| {
             test::TestRequest::post()
                 .uri("/api/bmi")
                 .set_json(payload)
                 .send_request(app)
         };
 
-        let check_body = |body: Value, expected_status: &str, expected_bmi: f64, expected_class: String| {
-            assert_eq!(body["status"], expected_status);
-            assert_eq!(body["data"]["bmi"].as_f64().unwrap(), expected_bmi);
-            assert_eq!(body["data"]["classification"], expected_class);
-        };
+        let check_body =
+            |body: Value, expected_status: &str, expected_bmi: f64, expected_class: String| {
+                assert_eq!(body["status"], expected_status);
+                assert_eq!(body["data"]["bmi"].as_f64().unwrap(), expected_bmi);
+                assert_eq!(body["data"]["classification"], expected_class);
+            };
         let app = test::init_service(App::new().configure(bmi_routes_init)).await;
 
         // Underweight
@@ -41,7 +40,12 @@ mod tests {
         assert!(resp.status().is_success());
 
         let body: Value = test::read_body_json(resp).await;
-        check_body(body, "success", 15.24, BmiClass::Underweight.as_str().to_string() );
+        check_body(
+            body,
+            "success",
+            15.24,
+            BmiClass::Underweight.as_str().to_string(),
+        );
 
         // Healthy
         let payload = json!({
@@ -53,8 +57,12 @@ mod tests {
         assert!(resp.status().is_success());
 
         let body: Value = test::read_body_json(resp).await;
-        check_body(body, "success", 21.22, BmiClass::Healthy.as_str().to_string() );
-
+        check_body(
+            body,
+            "success",
+            21.22,
+            BmiClass::Healthy.as_str().to_string(),
+        );
 
         //Overweight
         let payload = json!({
@@ -65,7 +73,12 @@ mod tests {
         let resp = send_req(&payload, &app).await;
 
         let body: Value = test::read_body_json(resp).await;
-        check_body(body, "success", 27.01, BmiClass::Overweight.as_str().to_string() );
+        check_body(
+            body,
+            "success",
+            27.01,
+            BmiClass::Overweight.as_str().to_string(),
+        );
 
         //Obese 1
         let payload = json!({
@@ -76,7 +89,12 @@ mod tests {
         let resp = send_req(&payload, &app).await;
 
         let body: Value = test::read_body_json(resp).await;
-        check_body(body, "success", 30.86, BmiClass::Obese1.as_str().to_string() );
+        check_body(
+            body,
+            "success",
+            30.86,
+            BmiClass::Obese1.as_str().to_string(),
+        );
 
         //Obese 2
         let payload = json!({
@@ -87,8 +105,12 @@ mod tests {
         let resp = send_req(&payload, &app).await;
 
         let body: Value = test::read_body_json(resp).await;
-        check_body(body, "success", 38.58, BmiClass::Obese2.as_str().to_string() );
-
+        check_body(
+            body,
+            "success",
+            38.58,
+            BmiClass::Obese2.as_str().to_string(),
+        );
 
         //Obese 3
         let payload = json!({
@@ -99,14 +121,16 @@ mod tests {
         let resp = send_req(&payload, &app).await;
 
         let body: Value = test::read_body_json(resp).await;
-        check_body(body, "success", 43.86, BmiClass::Obese3.as_str().to_string() );
-
-
+        check_body(
+            body,
+            "success",
+            43.86,
+            BmiClass::Obese3.as_str().to_string(),
+        );
     }
 
     #[actix_web::test]
     async fn test_invalid_params() {
-
         let app = test::init_service(App::new().configure(bmi_routes_init)).await;
         let payload = json!({
             "weight": -55,
@@ -116,14 +140,14 @@ mod tests {
         let resp = test::TestRequest::post()
             .uri("/api/bmi")
             .set_json(&payload)
-            .send_request(&app).await;
+            .send_request(&app)
+            .await;
 
         assert!(resp.status().is_client_error());
 
         let body: Value = test::read_body_json(resp).await;
         assert_eq!(body["status"], "error");
         assert_eq!(body["message"], "Invalid parameters");
-
 
         // Invalid weight
         let payload = json!({
@@ -134,7 +158,8 @@ mod tests {
         let resp = test::TestRequest::post()
             .uri("/api/bmi")
             .set_json(&payload)
-            .send_request(&app).await;
+            .send_request(&app)
+            .await;
 
         assert!(resp.status().is_client_error());
 
@@ -151,15 +176,13 @@ mod tests {
         let resp = test::TestRequest::post()
             .uri("/api/bmi")
             .set_json(&payload)
-            .send_request(&app).await;
+            .send_request(&app)
+            .await;
 
         assert!(resp.status().is_client_error());
 
         let body: Value = test::read_body_json(resp).await;
         assert_eq!(body["status"], "error");
         assert_eq!(body["message"], "Invalid parameters");
-
-
     }
-
 }
